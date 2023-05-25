1044e896928ec0489c1b56a8ed6f66c4477b960d
hafidh7 <hafidh_170798@yahoo.co.id>
2023-05-25 16:29:41 +0700

    add Sensor Types

---
+++ src/FlowSensor_Type.h
@@ -0,0 +1,11 @@
+// Sensor Type
+// -----------------------
+// YF
+#define YFS201	450
+#define YFB1	660
+// OF
+#define OF10ZAT	400
+#define OF10ZZT	400
+#define OF05ZAT	2174
+#define OF05ZZT	2174
+//------------------------
