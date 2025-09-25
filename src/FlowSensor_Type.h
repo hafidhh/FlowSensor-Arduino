4f012fe51ac4a585816958921891ce0a32b21815
Hafidh Hidayat <hafidh_170798@yahoo.co.id>
2025-09-25 09:52:10 +0700

    Update FlowSensor_Type.h
    fix typo

--- src/FlowSensor_Type.h
+++ src/FlowSensor_Type.h
@@ -13,19 +13,18 @@
 // Sensor Type
 // -----------------------
 // YF
-#define YFD50   20
-#define YFS201	450
-#define YFB5 450
-#define YFB1	660
-// YF-B10 - new 1" Flowsensor - A. Wilpernig 5.4.2025  F=(6*Q-8) ==> 352
-#define YFB10	352
-#define YFS401	5880
+#define YFDN50    20
+#define YFS201	 450
+#define YFB5     450
+#define YFB1     660
+#define YFB10    352
+#define YFS401  5880
 #define YFB1S   1077
 // OF
-#define OF10ZAT	400
-#define OF10ZZT	400
-#define OF05ZAT	2174
-#define OF05ZZT	2174
+#define OF10ZAT	 400
+#define OF10ZZT  400
+#define OF05ZAT 2174
+#define OF05ZZT 2174
 //------------------------
 
 /**
